fcb5ed71b33b9bea46814903f673b4c95fbef617
erfan <erfansooper@yahoo.com>
2023-04-20 19:10:33 +0200

    hi

--- federatedscope/vertical_fl/model/__init__.py
+++
@@ -1,3 +0,0 @@
-from federatedscope.vertical_fl.model.Tree import MultipleTrees
-
-__all__ = ['MultipleTrees']
--- federatedscope/vertical_fl/trainer/__init__.py
+++
@@ -1,3 +0,0 @@
-from federatedscope.vertical_fl.trainer.trainer import VerticalTrainer
-from federatedscope.vertical_fl.trainer.feature_order_protected_trainer \
-    import FeatureOrderProtectedTrainer
